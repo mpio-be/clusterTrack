9cc02dd31821191e28b51135bf1a1254bf47abf9
Mihai Valcu <valcu@orn.mpg.de>
2025-05-19 18:17:02 +0200

    work in progress...

--- R/cluster_track.R
+++ R/cluster_track.R
@@ -80,16 +80,32 @@ dbscan_ctdf <- function(ctdf, sd = 1, transform = log, minPts = 5, borderPoints 
 #' 
 cluster_track <- function(ctdf, sd = 1, transform = log, minPts = 5, borderPoints = TRUE) {
 
-  o = ctdf[, dbscan_ctdf(
-            ctdf = .SD,
-            sd = sd, 
-            transform = transform, 
-            minPts = minPts, 
-            borderPoints = borderPoints
-            ),
-              by = .segment
-            ]
-  o[, cluster := paste(.segment, cluster) |> as.factor() |> as.integer()]
+  segs = ctdf$.segment |> unique()
+
+  o = foreach(si = segs, .errorhandling = 'pass') %do% {
+    x = ctdf[.segment == si]
+    oi = dbscan_ctdf(
+      ctdf         = x,
+      sd           = sd,
+      transform    = transform,
+      minPts       = minPts,
+      borderPoints = borderPoints
+    )
+    oi[, cluster := paste(si, cluster)]
+    oi
+  }
+
+  e = sapply(o, FUN = inherits, what = "error")
+  
+  if (length(segs[e]) > 0) {
+    warning("segments ", paste(segs[e], collapse = ", "), " did not return a cluster!")
+  }
+
+  o = o[!e]
+
+  o = rbindlist(o)
+
+  o[, cluster := as.factor(cluster) |> as.integer()]
 
   # sanity check
   ncl = nrow(o[cluster>0, .N, cluster])
--- R/slice.R
+++ R/slice.R
@@ -6,14 +6,13 @@
 #' @examples
 #' data(toy_ctdf_k2)
 #' ctdf = as_ctdf(toy_ctdf_k2, crs = 4326, project_to = "+proj=eqearth")
-#' slice_ctdf(ctdf)
+#' slice_ctdf(ctdf, slice_by_distance = 4000)
 #' se = st_as_sf(ctdf)
 #' mapviewOptions(fgb = FALSE)
 #' mapview(se, zcol = ".filter")
 #' mapview(se, zcol = ".segment")
 #' 
-slice_ctdf <- function(ctdf, deltaT = 12, slice_min_n_segments = 5, slice_min_bout_length, filter_min_n_segments = 5) {
-
+slice_ctdf <- function(ctdf, deltaT = 12, slice_by_segments = 5, slice_by_distance=0, filter_by_segments = 5) {
 
   if (!inherits(ctdf, "ctdf")) {
     stop("slice_ctdf() only works on objects of class 'ctdf'")
@@ -44,22 +43,25 @@ slice_ctdf <- function(ctdf, deltaT = 12, slice_min_n_segments = 5, slice_min_bo
 
   segs[, bout_id := rleid(cross)]
   segs[, N_bout_id := .N, bout_id]
-  segs[, len := st_length(segment)]
+  segs[, len := st_length(segment) |> set_units("meters") |> as.numeric()]
+  segs[, len := sum(len), bout_id]
 
   # segmentation
-    doseg = segs[(!cross) & N_bout_id > slice_min_n_segments]
+    if(slice_by_distance == 0)
+      doseg = segs[(!cross) & N_bout_id > slice_by_segments ] else
+      doseg = segs[(!cross) & (N_bout_id > slice_by_segments | len >= slice_by_distance)]
+      
     doseg[, bout_id := factor(bout_id) |> as.integer()]
+    
     # mid segments timestamps
     midseg = ctdf[ doseg[, round(median(.id) ), by = bout_id]$V1 ]$timestamp
 
   # filter
-    dofill = segs[N_bout_id >=filter_min_n_segments,]$.id
+    dofill = segs[N_bout_id >=filter_by_segments,]$.id
 
   # set
     ctdf[, .segment := findInterval(timestamp, midseg)]
     ctdf[.id %in% dofill, .filter := TRUE]
-
-
 
 
 }
