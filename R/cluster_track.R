1549e5159c805c4452abfae1609c55a2185b2410
Mihai Valcu <valcu@orn.mpg.de>
2025-07-10 16:38:56 +0200

    fix: cosmetics

--- R/cluster_track.R
+++ R/cluster_track.R
@@ -10,7 +10,6 @@ print.clusterTrack <- function(x, ...) {
     print.keys= FALSE,
     ...)
 
-
 }
 
 #' @export
@@ -21,7 +20,6 @@ plot.clusterTrack <- function(x) {
   plot(st_geometry(x$location), col = cols)
 }
 
-
 #' Cluster movement tracks
 #'
 #' Performs spatiotemporal clustering on a ctdf by segmenting movement, identifying stops, and applying DBSCAN-like clustering.
@@ -31,7 +29,7 @@ plot.clusterTrack <- function(x) {
 #'
 #'
 #' @param ctdf   A `ctdf` data frame (see [as_ctdf()]) representing a single movement track .
-#' @param deltaT Numeric; maximum allowable gap (in days) between segment endpoints to consider them continuous. 
+#' @param deltaT Numeric; maximum allowable gap (in days) between segment endpoints to consider them continuous.
 #'               Default to 1 day. Passed to [slice()]) .
 #' @param nmin   Integer. Segments or tessellations with fewer than nmin points yield no clusters.
 #'               Default to 3. Passed to [cluster_segments()].
@@ -40,43 +38,39 @@ plot.clusterTrack <- function(x) {
 #'                  If `method = "quantile"`, interpreted as the multiplier of the standard deviation
 #'                  on log‐areas used in pruning. Passed to [cluster_segments()].
 #' @param method Character, one of `"sd"` or `"quantile"`. Determines the pruning
-#'               strategy in the tessellation step:  
-#'               - `"sd"`: prune cells with area < quantile(polygon_area, probs = threshold)  
+#'               strategy in the tessellation step:
+#'               - `"sd"`: prune cells with area < quantile(polygon_area, probs = threshold)
 #'               - `"quantile"`: prune cells with log(area) ≤ mean(log(area)) + threshold * sd(log(area)).
 #'               Passed to [cluster_segments()].
 #' @param time_contiguity Logical; if `TRUE`, missing cluster IDs are forward‐filled.
-#'                        and backward‐filled within each segment to enforce temporal continuity. 
+#'                        and backward‐filled within each segment to enforce temporal continuity.
 #'                        Default to `FALSE`.
 #'                        Passed to [cluster_segments()].
 #' #' @param overlap_threshold Numeric between 0 and 1; minimum area‐overlap ratio
 #'                          required to merge adjacent clusters. Default to 0.1.
 #'                          Clusters with overlap > threshold are combined.
 #'                          Passed to [stitch_cluster()]
-
-
 #' @return NULL. The function modifies `ctdf` by reference, adding or updating the column \code{cluster}, which assigns a cluster ID to each row (point).
 #'
 
 #' @export
 #' @examples
 #' data(toy_ctdf_k2)
-#' ctdf = as_ctdf(toy_ctdf_k2, s_srs = 4326, t_srs = "+proj=eqearth")
+#' ctdf = as_ctdf(toy_ctdf_k2)
 #' cluster_track(ctdf)
-#' 
+#' \dontrun{
 #' data(pesa56511)
-#' ctdf  = as_ctdf(pesa56511, time = "locationDate", s_srs = 4326, t_srs = "+proj=eqearth")
+#' ctdf = as_ctdf(pesa56511, time = "locationDate")
 #' cluster_track(ctdf)
-#' 
+#'
 #' data(lbdo66867)
-#' ctdf = as_ctdf(lbdo66867, time = "locationDate", s_srs = 4326, t_srs = "+proj=eqearth")
+#' ctdf = as_ctdf(lbdo66867, time = "locationDate")
 #' cluster_track(ctdf)
-#' 
+#'
 #' data(lbdo66862)
-#' ctdf = as_ctdf(lbdo66862, time = "locationDate", s_srs = 4326, t_srs = "+proj=eqearth")
+#' ctdf = as_ctdf(lbdo66862, time = "locationDate")
 #' cluster_track(ctdf)
-#' 
-#' 
-
+#' }
 
 cluster_track <- function(ctdf,deltaT = 1, nmin = 3, threshold = 0.90, method = "quantile", time_contiguity = FALSE, overlap_threshold = 0.1) {
 
