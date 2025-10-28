c36f3bcaa7bea71f855d9f4def8d3cb85f1afcc7
leberhartphillips <luke.eberhart@gmail.com>
2025-10-28 08:18:13 +0100

    debugged cluster_stitch() to allow single clusters

--- R/stitch.R
+++ R/stitch.R
@@ -2,22 +2,22 @@
 .stitch <- function(ctdf,  overlap_threshold = 0) {
 
   o = ctdf[cluster > 0, .(hull = st_union(location) |> st_convex_hull()), by = cluster] |> setDT()
-  
+
   o[, next_hull := hull[c(2:.N, NA_integer_)] ]
-  
+
   o[, overlap := .st_area_overlap_ratio(hull, next_hull), by = cluster]
   o[.N, overlap:= 0]
   o[, is_overlap := overlap > overlap_threshold]
 
   o[ , stitch_id := rleid(is_overlap)]
   o[(!is_overlap), stitch_id := NA]
-  
+
   o[, stitch_id := fcoalesce(stitch_id, shift(stitch_id))]
 
   o[, grp_key := fifelse(
       !is.na(stitch_id),
-      paste0("s", stitch_id),  
-      paste0("c", cluster)       
+      paste0("s", stitch_id),
+      paste0("c", cluster)
   )]
 
   o[, cluster_stitched := .GRP, by = grp_key]
@@ -38,7 +38,7 @@
 #'
 #' @param ctdf A CTDF object. Must contain an updated `cluster` column.
 #' @param overlap_threshold Numeric between 0 and 1; minimum area‐overlap ratio
-#'                          required to merge adjacent clusters. 
+#'                          required to merge adjacent clusters.
 #'                          Clusters with overlap > threshold are combined.
 #'#' @return The input CTDF, with an updated (in-place) cluster` column.
 #'
@@ -52,10 +52,10 @@
 
 #' data(pesa56511)
 #' ctdf  = as_ctdf(pesa56511, time = "locationDate", s_srs = 4326, t_srs = "+proj=eqearth")
-#' slice_ctdf(ctdf) 
+#' slice_ctdf(ctdf)
 #' cluster_segments(ctdf)
 #' cluster_stitch(ctdf)
-#' 
+#'
 #' data(lbdo66862)
 #' ctdf = as_ctdf(lbdo66862, time = "locationDate", s_srs = 4326, t_srs = "+proj=eqearth")
 #' slice_ctdf(ctdf)
@@ -65,14 +65,14 @@
 
 
 cluster_stitch <- function(ctdf,  overlap_threshold = 0.1) {
-  
+
   .check_ctdf(ctdf)
 
-    
+
   repeat {
     n_prev = max(ctdf$cluster)
     .stitch(ctdf, overlap_threshold = overlap_threshold)
-    if (max(ctdf$cluster) == n_prev) break
+    if (max(ctdf$cluster) == n_prev | max(ctdf$cluster) < n_prev) break
   }
 
 }
